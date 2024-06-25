8e397b20804ea943dbfdb30244d435b5aed7c3e2
Hsin Yen Chung <stevenchung@Hsins-MacBook-Pro-4.local>
2024-06-24 17:18:33 -0700

    rm examlpe code

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,3 @@
 export async function GET() {
-  return Response.json({
-    files: [
-      {
-        name: "Xbot.glb",
-        type: "animation/gltf",
-        baseActions: {
-          idle: { weight: 1 },
-          walk: { weight: 0 },
-          run: { weight: 0 },
-        },
-      },
-    ],
-  });
+  return Response.json({});
 }
